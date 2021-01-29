aab7157a9824b5cdf71fbc05603d95a8452839f6
Michael Walters <mikel.walters@gmail.com>
2021-01-29 14:56:16 -0500

    fixed boolean issue

--- driver/mssql.platform.ts
+++ driver/mssql.platform.ts
@@ -1,6 +1,24 @@
 import { AbstractSqlPlatform } from '@mikro-orm/knex';
 import { MsSqlSchemaHelper } from './mssql.schemaHelper';
 
+function isBoolean(value: any) {
+    return typeof value == "boolean";
+}
+
+function booleanToBit(value: boolean) {
+    return value ? 1 : 0;
+}
+
+function formatParam(value: any) {
+    if (isBoolean(value))
+        return booleanToBit(value);
+    return value;
+}
+
 export class MsSqlPlatform extends AbstractSqlPlatform {
     protected schemaHelper = new MsSqlSchemaHelper();
+
+    formatQuery(sql: string, params: readonly any[]): string {
+        return super.formatQuery(sql, params?.map(formatParam));
+    }
 }
