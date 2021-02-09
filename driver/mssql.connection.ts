c4dbc1a208d0aed8cbb7578b06c088cf656d3968
Michael Walters <mikel.walters@gmail.com>
2021-02-09 13:42:01 -0500

    update inserts

--- driver/mssql.connection.ts
+++ driver/mssql.connection.ts
@@ -1,5 +1,5 @@
+import { EntityMetadata, Transaction } from '@mikro-orm/core';
 import { AbstractSqlConnection } from '@mikro-orm/knex';
-import { isIn } from 'class-validator';
 import dialect from 'knex/lib/dialects/mssql';
 
 export class MsSqlConnection extends AbstractSqlConnection {
@@ -35,20 +35,50 @@ export class MsSqlConnection extends AbstractSqlConnection {
   }
 
   private getPatchedDialect() {
-    const INSERT_RE = /^insert into/;
-    const isInsert = (obj) => obj?.method == "raw" && obj?.sql?.trim().match(INSERT_RE);
-    const processResponse = dialect.prototype.processResponse;
-    dialect.prototype.processResponse = function (obj, runner) {
-      if (isInsert(obj)) {
-        return obj.response;
-      }
-      return processResponse(obj, runner);
-    }
+    // const _processResponse = dialect.prototype.processResponse;
+    // dialect.prototype.processResponse = function (obj, runner) {
+    //   return _processResponse(obj, runner);
+    // }
 
-    const query = dialect.prototype._query;
-    dialect.prototype._query = function (connection, obj) {
-      return query(connection, obj);
-    }
+    // const _query = dialect.prototype._query;
+    // dialect.prototype._query = (connection, obj) => {
+    //   return _query(connection, obj);
+    // }
     return dialect;
+  }
+
+  execute<T>(
+    queryOrKnex: any,
+    params?: any[],
+    method?: 'all' | 'get' | 'run',
+    ctx?: Transaction
+  ): Promise<T> {
+    if (method === 'run' && typeof queryOrKnex === 'string')
+      queryOrKnex = this.patchInsert(queryOrKnex);
+    return super.execute(queryOrKnex, params, method, ctx);
+  }
+
+  private patchInsert(sql: string) {
+    const [results] = Array.from(sql.matchAll(/^insert into (.*) (\(.*\)) values (\(.*\))/));
+    if (!results)
+      return sql;
+
+    const [_, table, columns, values] = results;
+    const collection = table.replace(/\[|\]/gm, '');
+    const { name: entityName } = Object.values(this.metadata.getAll()).find((meta: EntityMetadata) => meta.collection === collection);
+    const metadata = this.metadata.get(entityName);
+
+    const getPk = (pk: string) => metadata.properties[pk];
+    const getPkFieldName = (pk: string) => getPk(pk).fieldNames[0];
+    const getPkColumnType = (pk: string) => getPk(pk).columnTypes[0];
+    const output = metadata.primaryKeys.map(pk => `inserted.[${getPkFieldName(pk)}]`);
+    const tableColumns = metadata.primaryKeys.map(pk => `${getPkFieldName(pk)} ${getPkColumnType(pk)}`).join(', ');
+    const selectColumns = metadata.primaryKeys.map(pk => `[${getPkFieldName(pk)}] as ${pk}`).join(", ");
+
+    const declare = `declare @${entityName} table (${tableColumns})`;
+    const insertInto = `insert into ${table} ${columns} output ${output} into @${entityName} values ${values}`;
+    const select = `select ${selectColumns} from @${entityName}`;
+    const value = `${declare}; ${insertInto}; ${select};`;
+    return value;
   }
 }
